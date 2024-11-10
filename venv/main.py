7b8e392c74989795eba0a7852c81fb4d6e3d87c5
hacnguyen <vanhacnguyen1708@gmail.com>
2024-11-09 20:22:49 -0800

    add function subtract

--- venv/main.py
+++ venv/main.py
@@ -1,51 +1,45 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
-from responses import get_response
+from discord import Intents, Client, Message, Interaction
+from discord.ext import commands
+
+# import sympy as smp
+# import numpy as np
+
+#find api, dm joshiro for setting up get response
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-
+CHANNEL_ID = # place channel ID
 # Bot setup
-intents: Intents = Intents.default()
-intents.message_content = True
-client: Client = Client(intents = intents)
-
-# Message functionality
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print("(Message was empty because intents were not enabled properly)")
-        return
-    
-    # Provide a way for bot message user privately
-    if is_prinvate := user_message[0] == '?': # walrus operator
-        user_message = user_message[1:] # Slice to exclude the ?
-
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_prinvate else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+bot_intents: Intents = Intents.default()
+bot_intents.message_content = True
+client = commands.Bot(command_prefix = '!', intents = bot_intents) # Can change the command_prefix, this is just what goes in front of a command like !ping
 
 # Handling the startup for bot
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send("Hello! Mad_Scientist is ready!")
 
-# Handling incoming messages
-@client.event
-async def on_message(message: Message) -> None:
-    # if the bot is the one writing message 
-    if message.author == client.user:
-        return
-    
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
+@client.command()
+async def add(ctx, *arr):
+    result = 0
+    for i in arr:
+        if i.isdigit():
+            result += int(i)
+    await ctx.send(f"Answer: {result}")
 
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
+
+@client.command()
+async def subtract(ctx, *arr):
+    result = 0
+    for i in arr:
+        if i.isdigit():
+            result -= int(i)
+    await ctx.send(f"Answer: {result}")
 
 # Main entry point
 def main() -> None:
