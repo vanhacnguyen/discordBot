346d5dfbcf41eddadcd82f69aa5c0c5a20147095
hacnguyen <vanhacnguyen1708@gmail.com>
2024-11-27 20:47:20 -0800

    add integrals and graphing

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message, Interaction
 from discord.ext import commands
+import discord
 
+import io
 import sympy as smp
 import numpy as np
 import matplotlib.pyplot as plt
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-CHANNEL_ID = # place channel ID
+CHANNEL_ID = # place channel ID here
 # Bot setup
 bot_intents: Intents = Intents.default()
 bot_intents.message_content = True
@@ -23,28 +25,27 @@ async def on_ready() -> None:
     channel = client.get_channel(CHANNEL_ID)
     await channel.send("Hello! Mad_Scientist is ready!")
 
-# Helper method to insert '*' to x
-def insertTimeX(eq):
-    # Process each occurrence of 'x'
-    x_index = eq.find("x")  # Find the first occurrence
+# helper method to convert tuple to a string and insert '*' to x
+def convert_eq_str(eq):
+    eq_str = " ".join(eq).replace("sinx", "sin(x)").replace("cosx", "cos(x)").replace("tanx", "tan(x)").replace("^", "**")
+    #process each occurrence of 'x'
+    x_index = eq_str.find("x")  #find the first occurrence
     while x_index != -1: 
-        # Check the character before 'x'
-        if x_index > 0:  # Ensure it's not the first character
-            char_before = eq[x_index - 1]
-            if char_before.isdigit():  # If a digit is in front of x, add '*'
-                eq = eq[:x_index] + "*x" + eq[x_index + 1:]
-                x_index += 1  # Move index forward to account for inserted '*'
-        # Find the next occurrence of 'x'
-        x_index = eq.find("x", x_index + 1)
-    return eq
+        #check the character before 'x'
+        if x_index > 0:  #not the first character
+            char_before = eq_str[x_index - 1]
+            if char_before.isdigit():  #if a digit is in front of x, add '*'
+                eq_str = eq_str[:x_index] + "*x" + eq_str[x_index + 1:]
+                x_index += 1  #move index forward to account for inserted '*'
+        #find the next occurrence of 'x'
+        x_index = eq_str.find("x", x_index + 1)
+    return eq_str
 
 @client.command()
 # *equation is a tuple of strings
 async def solve(ctx, *equation):
     try:
-        eq_str = " ".join(equation).replace("^", "**") # combine into a single string
-
-        eq_str = insertTimeX(eq_str)
+        eq_str = convert_eq_str(equation) # combine into a single string
         x = smp.symbols('x', real = True)
 
         if "=" in eq_str:
@@ -62,9 +63,7 @@ async def solve(ctx, *equation):
 @client.command()
 async def factor(ctx, *expr):
     try:
-        eq_str = " ".join(expr).replace("^", "**")
-
-        eq_str = insertTimeX(eq_str)
+        eq_str = convert_eq_str(expr)
         eq = smp.parse_expr(eq_str)
         answer = smp.factor(eq)
         answer = answer.replace("**", "^")
@@ -75,21 +74,20 @@ async def factor(ctx, *expr):
 @client.command()
 async def limit(ctx, *expr):
     try:
-        eq_str = " ".join(expr).replace("^", "**")
+        eq_str = convert_eq_str(expr)
         x = smp.symbols('x', real = True)
 
-        eq_str = insertTimeX(eq_str)
-
-        # Find the arrow to find limit point
+        #find the arrow to find limit point
         arrow_index = eq_str.index(">")
-        limit_expr_str = eq_str[:arrow_index].strip() # Get the expression before arrow
-        limit_point = eq_str[arrow_index + 1:].strip() # Get the limit point 
+        limit_expr_str = eq_str[:arrow_index].strip() #get the expression before arrow
+        limit_point = eq_str[arrow_index + 1:].strip() #get the limit point 
+        parsed_expr = smp.parse_expr(limit_expr_str, {"sin": smp.sin, "cos": smp.cos, "tan": smp.tan})
         if '+' in limit_point:
-            limit_expr = smp.limit(smp.parse_expr(limit_expr_str), x, limit_point.replace("+", ""), "+")
+            limit_expr = smp.limit(parsed_expr, x, limit_point.replace("+", ""), "+")
         elif '-' in limit_point:
-            limit_expr = smp.limit(smp.parse_expr(limit_expr_str), x, limit_point.replace("-", ""), "-")
+            limit_expr = smp.limit(parsed_expr, x, limit_point.replace("-", ""), "-")
         else:
-            limit_expr = smp.limit(smp.parse_expr(limit_expr_str), x, limit_point)
+            limit_expr = smp.limit(parsed_expr, x, limit_point)
 
         limit_expr_str = limit_expr_str.replace("**", "^")
         limit_expr = limit_expr.replace("**", "^")
@@ -101,16 +99,65 @@ async def limit(ctx, *expr):
 async def derivative(ctx, *expr):
     try:
         x = smp.symbols('x')
-        eq_str = " ".join(expr).replace("^", "**")
-        eq_str = insertTimeX(eq_str)
-        expr_diff = smp.Derivative(smp.parse_expr(eq_str), x)
+        eq_str = convert_eq_str(expr)
+        #parse sin(x), cos(x), tan(x) into sympy expression
+        parsed_expr = smp.parse_expr(eq_str, {"sin": smp.sin, "cos": smp.cos, "tan": smp.tan})
+
+        expr_diff = smp.Derivative(parsed_expr, x)
         answer = expr_diff.doit()
-        answer = answer.replace("**", "^")
-        eq_str = eq_str.replace("**", "^")
-        await ctx.send(f"{eq_str}: " + str(answer))
+        eq_str = eq_str.replace("**", "^").replace("*","")
+        pretty_answer = smp.pretty(answer).replace("*", "") #pretty print to read
+
+        #back-tick for discord code format
+        await ctx.send(f"Derivative of {eq_str}: \n```{pretty_answer}```")
     except Exception as e:  
         await ctx.send(f"Error finding derivative: {str(e)}")
 
+@client.command()
+async def integrate(ctx, *expr):
+    try:
+        x = smp.symbols('x')
+        expr_str = convert_eq_str(expr)
+        parsed_expr = smp.parse_expr(expr_str, {"sin": smp.sin, "cos": smp.cos, "tan": smp.tan})
+        expr_integral = smp.integrate(parsed_expr, x)
+        pretty_answer = smp.pretty(expr_integral).replace("*", "")
+        expr_str = expr_str.replace("**", "^").replace("*","")
+        await ctx.send(f"Integrals of {expr_str}: \n```{pretty_answer}```")
+    except Exception as e:
+        await ctx.send(f"Error finding integrals: {str(e)}")
+@client.command()     
+async def graphEquation(ctx, *equation):
+    try:
+        eq_str = convert_eq_str(equation)
+        try: 
+            parsed_expr = smp.parse_expr(eq_str)
+        except smp.SympifyError as e:
+            await ctx.send(f"Invalid equation. Error {str(e)}")
+            return
+        x = smp.symbols('x')
+        x_vals = np.linspace(-10, 10, 100)
+
+        expr_f = smp.lambdify(x, parsed_expr) #convert sympy expressions into python functions
+        y_vals = expr_f(x_vals) #evaluate each x_vals for plotting y
+
+        plt.figure()
+        plt.plot(x_vals, y_vals, label=f"${eq_str}$")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.axhline(0, color = "black", linewidth = 0.5, linestyle = "--")
+        plt.axvline(0, color = "black", linewidth = 0.5, linestyle = "--")
+        plt.grid(True)
+        plt.legend()
+
+        buffer = io.BytesIO() #hold image data in memory temporarily instead of saving file
+        plt.savefig(buffer, format='png') #hold image data into the buffer
+        buffer.seek(0) #reset the cursor to the beginning
+        #send image
+        await ctx.send(file=discord.File(fp=buffer, filename='graph.png'))
+        buffer.close()
+        plt.close()
+    except Exception as e:
+        await ctx.send("Error graphing:" + str(e))
 # Main entry point
 def main():
     client.run(TOKEN)
